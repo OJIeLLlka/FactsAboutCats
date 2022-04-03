19b5bbf4ab44ff063683880324332dfc228f4e1f
OJIeLLlka <olbastnie@gmail.com>
2022-04-03 11:06:47 +0300

    Initial commit

---
+++ src/main/java/Cats.java
@@ -0,0 +1,75 @@
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Cats {
+
+    private String id;
+    private String text;
+    private String type;
+    private String user;
+    private String upvotes;
+
+    @Override
+    public String toString() {
+        return "Cats{" +
+                "id='" + id + '\'' +
+                ", text='" + text + '\'' +
+                ", type='" + type + '\'' +
+                ", user='" + user + '\'' +
+                ", upvotes='" + upvotes + '\'' +
+                '}';
+    }
+
+    public Cats(
+            @JsonProperty("id") String id,
+            @JsonProperty("text") String text,
+            @JsonProperty("type") String type,
+            @JsonProperty("user") String user,
+            @JsonProperty("upvotes") String upvotes
+    ) {
+        this.id = id;
+        this.text = text;
+        this.type = type;
+        this.user = user;
+        this.upvotes = upvotes;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public void setUpvotes(String upvotes) {
+        this.upvotes = upvotes;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public String getUpvotes() {
+        return upvotes;
+    }
+}
